316966f6e88be2c129c71bce25568e3e2d3d3c56
Yasmeen saleh <lap top@DESKTOP-40R4L6L>
2026-05-04 15:07:33 +0300

    remove project

--- Authentication and Authorization Task/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Authentication_and_Authorization_Task.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
